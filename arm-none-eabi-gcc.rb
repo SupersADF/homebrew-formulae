8e890c424750d0d3db65c4609d9dcf0b2c2538ff
Supers <supers.ya@yandex.ru>
2019-01-08 18:44:29 +0300

    Update arm-none-eabi-gcc.rb
    update version GCC

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -3,9 +3,9 @@ require 'formula'
 class ArmNoneEabiGcc < Formula
 
   homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads'
-  version '7-2018-q2-update'
+  version '8-2018-q4-major'
 
-  url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/7-2018q2/gcc-arm-none-eabi-7-2018-q2-update-mac.tar.bz2'
+  url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/8-2018q4/gcc-arm-none-eabi-8-2018-q4-major-mac.tar.bz2?revision=1041bf49-06d4-4174-866f-0e5259fa9d8d?product=GNU%20Arm%20Embedded%20Toolchain,64-bit,,Mac%20OS%20X,8-2018-q4-major'
   sha256 'c1c4af5226d52bd1b688cf1bd78f60eeea53b19fb337ef1df4380d752ba88759'
 
   def install
