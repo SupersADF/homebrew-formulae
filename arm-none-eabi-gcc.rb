1dff50bb08d1a22bf87b9a8c15321a40f3bfadfc
Supers <supers.ya@yandex.ru>
2019-01-08 18:48:08 +0300

    update version GCC and sha256

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -6,7 +6,7 @@ class ArmNoneEabiGcc < Formula
   version '8-2018-q4-major'
 
   url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/8-2018q4/gcc-arm-none-eabi-8-2018-q4-major-mac.tar.bz2?revision=1041bf49-06d4-4174-866f-0e5259fa9d8d?product=GNU%20Arm%20Embedded%20Toolchain,64-bit,,Mac%20OS%20X,8-2018-q4-major'
-  sha256 'c1c4af5226d52bd1b688cf1bd78f60eeea53b19fb337ef1df4380d752ba88759'
+  sha256 '0b528ed24db9f0fa39e5efdae9bcfc56bf9e07555cb267c70ff3fee84ec98460'
 
   def install
     (prefix/"gcc").install Dir["./*"]
